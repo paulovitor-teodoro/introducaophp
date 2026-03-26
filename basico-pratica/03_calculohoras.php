51f805ad6501b22b6cdb2587dedf12c004a699cd
Paulo Vitor <Teodoropaulo1504@gmail.com>
2026-03-26 20:38:43 -0300

    correção

--- basico-pratica/03_calculohoras.php
+++ basico-pratica/03_calculohoras.php
@@ -3,19 +3,20 @@
  $horaSaida = new DateTime('18:00');
 
  $interval = $horaSaida->diff($horaEntrada);
+
  $horasTrabalhadas = $interval->h;
- echo "Horas trabalhadas: " . $horasTrabalhadas;
-?>
+
+ ?>
 <!DOCTYPE html>
 <html lang="pt-br">
-
+    
     <head>
         <meta charset="UTF-8">
         <title>Cálculo de Horas</title>
     </head>
     <body>
         <?php
-        
+        echo "Horas trabalhadas: " . $horasTrabalhadas;
         ?>
     </body>
 
