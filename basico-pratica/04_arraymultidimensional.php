58e33aa88354b1116cfe08d5e3801e1d6e9ec091
Paulo Vitor <Teodoropaulo1504@gmail.com>
2026-03-26 22:09:02 -0300

    Alterações

--- basico-pratica/04_arraymultidimensional.php
+++ basico-pratica/04_arraymultidimensional.php
@@ -1,8 +1,13 @@
 <?php
- $Estados = [
-     ['SP' => 'São Paulo'],
-     ['RJ' => 'Rio de Janeiro'],
-     ['MG' => 'Minas Gerais']
+//  $Estados = [
+//     ['SP' => 'São Paulo'],
+//     ['RJ' => 'Rio de Janeiro'],
+//     ['MG' => 'Minas Gerais']
+//  ];
+ $estados = [
+     ['SP','São Paulo'],
+     ['RJ', 'Rio de Janeiro'],
+     ['MG', 'Minas Gerais']
  ];
 ?>
 <!DOCTYPE html>
@@ -14,11 +19,15 @@
     </head>
     <body>
         <?php
-        foreach ($Estados as $estado) {
-        foreach ($estado as $uf => $nome) {
-        echo "UF: $uf - Estado: $nome <br>";
-    }
-}
+//foreach ($Estados as $estado) {
+// foreach ($estado as $uf => $nome) {
+// echo "UF: $uf - Estado: $nome <br>";
+//  }
+// }
+        for($i = 0; $i < count($estados); $i++){
+            echo "UF: {$estados[$i][0]} - Estado: {$estados[$i][1]}" . "<br>";
+        }
+        
         ?>
         
     </body>
